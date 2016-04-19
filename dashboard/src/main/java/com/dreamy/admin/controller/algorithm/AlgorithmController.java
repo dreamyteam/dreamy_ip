9c5e5c39afdb8f8c35147127ce213cb6ac6b9c22
jiangshan <yongxing@dreamy.live>
2016-04-19 20:41:34 +0800

    commit

--- dashboard/src/main/java/com/dreamy/admin/controller/algorithm/AlgorithmController.java
+++ dashboard/src/main/java/com/dreamy/admin/controller/algorithm/AlgorithmController.java
@@ -3,10 +3,13 @@ package com.dreamy.admin.controller.algorithm;
 import com.dreamy.admin.controller.DashboardController;
 import com.dreamy.beans.Page;
 import com.dreamy.domain.ipcool.Algorithm;
+import com.dreamy.enums.AlgorithmEnums;
 import com.dreamy.service.iface.ipcool.AlgorithmService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -30,5 +33,20 @@ public class AlgorithmController extends DashboardController {
 
         return "/algorithm/algorithm-list";
 
+    }
+
+    @RequestMapping(value = "/edit")
+    public String insert(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id, ModelMap model) {
+
+
+        model.put("type", AlgorithmEnums.values());
+        return "/algorithm/algorithm-edit";
+    }
+
+    @RequestMapping(value = "/view")
+    public String view(@RequestParam(value = "id", required = true) Integer id, ModelMap model) {
+        Algorithm algorithm=algorithmService.getById(id);
+        model.put("algorithm",algorithm);
+        return "/algorithm/algorithm-view";
     }
 }
