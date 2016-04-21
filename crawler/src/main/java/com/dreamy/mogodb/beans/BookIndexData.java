eab2e59ab9903c5dfc76f53fb5a19d4c99f9d1f4
jiangshan <yongxing@dreamy.live>
2016-04-21 10:43:13 +0800

    commit

--- crawler/src/main/java/com/dreamy/mogodb/beans/BookIndexData.java
+++
@@ -1,7 +0,0 @@
-package com.dreamy.mogodb.beans;
-
-/**
- * Created by wangyongxing on 16/4/18.
- */
-public class BookIndexData {
-}
---
+++ servers/src/main/java/com/dreamy/mogodb/beans/BookIndexData.java
@@ -0,0 +1,20 @@
+package com.dreamy.mogodb.beans;
+
+import java.lang.ref.PhantomReference;
+
+/**
+ * Created by wangyongxing on 16/4/18.
+ */
+public class BookIndexData {
+
+    private String overviewJson;
+
+    private String soIndexJson;
+
+    private String soMediaJson;
+
+    private  String drawAreaJson;
+
+    private  String portrayalJson;
+
+}
