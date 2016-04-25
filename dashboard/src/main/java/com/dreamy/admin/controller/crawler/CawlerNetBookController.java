1d147aa9d52f28888a5c27963ec342b5316fa672
jiangshan <yongxing@dreamy.live>
2016-04-25 16:45:20 +0800

    commit

--- dashboard/src/main/java/com/dreamy/admin/controller/crawler/CawlerNetBookController.java
+++ dashboard/src/main/java/com/dreamy/admin/controller/crawler/CawlerNetBookController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by wangyongxing on 16/4/19.
  */
 @RequestMapping(value = "/crawler/index")
-public class CawlerIndexController {
+public class CawlerNetBookController {
 }
---
+++ dashboard/src/main/java/com/dreamy/admin/controller/crawler/PopularController.java
@@ -0,0 +1,7 @@
+package com.dreamy.admin.controller.crawler;
+
+/**
+ * Created by wangyongxing on 16/4/25.
+ */
+public class PopularController {
+}
