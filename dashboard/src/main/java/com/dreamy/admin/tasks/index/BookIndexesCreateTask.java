5e610b7a12a2fe0d88d131effd377fb0892d95d0
jeff <jeff@dreamy.live>
2016-04-28 14:52:38 +0800

    update : 修复首页尾部和一般尾部页面的冲突问题

---
+++ dashboard/src/main/java/com/dreamy/admin/tasks/index/BookIndexesCreateTask.java
@@ -0,0 +1,19 @@
+package com.dreamy.admin.tasks.index;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yujianfu (yujianfu@duotin.com)
+ * Date: 16/4/28
+ * Time: 下午2:17
+ */
+@Component
+public class BookIndexesCreateTask {
+
+    @Scheduled(fixedDelay = 8000)
+    private void run() {
+
+    }
+}
