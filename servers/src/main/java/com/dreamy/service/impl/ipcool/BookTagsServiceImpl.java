66a0b378229cec8929f39180db2499ee08a7c868
jeff <jeff@dreamy.live>
2016-05-05 10:20:52 +0800

    add : 完成ip的tag展示

--- servers/src/main/java/com/dreamy/service/impl/ipcool/BookTagsServiceImpl.java
+++ servers/src/main/java/com/dreamy/service/impl/ipcool/BookTagsServiceImpl.java
@@ -5,12 +5,18 @@ import com.dreamy.dao.iface.ipcool.BookTagsMapDao;
 import com.dreamy.domain.ipcool.BookTags;
 import com.dreamy.domain.ipcool.BookTagsConditions;
 import com.dreamy.domain.ipcool.BookTagsMap;
+import com.dreamy.domain.ipcool.BookTagsMapConditions;
+import com.dreamy.mogodb.beans.Book;
 import com.dreamy.service.iface.ipcool.BookTagsService;
 
+import com.dreamy.utils.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,12 +39,41 @@ public class BookTagsServiceImpl implements BookTagsService {
     }
 
     @Override
-    public List<BookTags> queryByName(String name) {
+    public List<BookTags> getByName(String name) {
 
         BookTagsConditions conditions = new BookTagsConditions();
-        conditions.createCriteria().andNameLike(name);
+        conditions.createCriteria().andNameEqualTo(name);
 
         return bookTagsDao.selectByExample(conditions);
+    }
+
+    @Override
+    public Map<Integer, String> getTagMapByBookId(Integer bookId) {
+
+        Map<Integer, String> tagIdMap = new HashMap<>();
+
+        BookTagsMapConditions mapConditions = new BookTagsMapConditions();
+        mapConditions.createCriteria().andBookIdEqualTo(bookId);
+        List<BookTagsMap> bookTagsMapList = bookTagsMapDao.selectByExample(mapConditions);
+        if (CollectionUtils.isNotEmpty(bookTagsMapList)) {
+
+            List<Integer> tagIds = new LinkedList<>();
+            for (BookTagsMap bookTagsMap : bookTagsMapList) {
+                tagIds.add(bookTagsMap.getTagId());
+            }
+
+            BookTagsConditions conditions = new BookTagsConditions();
+            conditions.createCriteria().andIdIn(tagIds);
+
+            List<BookTags> bookTagsList = bookTagsDao.selectByExample(conditions);
+            if (CollectionUtils.isNotEmpty(bookTagsList)) {
+                for (BookTags tag : bookTagsList)
+                    tagIdMap.put(tag.getId(), tag.getName());
+            }
+
+        }
+
+        return tagIdMap;
     }
 
     @Override
