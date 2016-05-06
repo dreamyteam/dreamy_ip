e5fe4edac78262294c59ae6974ebcc6086ffdf94
jiangshan <yongxing@dreamy.live>
2016-05-06 13:12:39 +0800

    add 微信关键词

--- crawler/src/main/java/com/dreamy/listen/KeyWordEventQueueHandler.java
+++ crawler/src/main/java/com/dreamy/listen/KeyWordEventQueueHandler.java
@@ -1,12 +1,18 @@
 package com.dreamy.listen;
 
 import com.alibaba.fastjson.JSONObject;
+import com.dreamy.domain.ipcool.KeyWord;
 import com.dreamy.handler.keyword.KeyWordHandler;
 import com.dreamy.utils.NumberUtils;
+import com.dreamy.utils.TimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by wangyongxing on 16/4/28.
@@ -19,18 +25,32 @@ public class KeyWordEventQueueHandler extends AbstractQueueHandler {
     @Autowired
     KeyWordHandler keyWordHandler;
 
+    private Date lastRuntime = null;
+
     @Override
     public void consume(JSONObject jsonObject) {
-        //获取类型
-        Integer type = jsonObject.getInteger("source");
-        Integer ipId = jsonObject.getInteger("bookId");
-        String word = jsonObject.getString("word");
-        try {
-            keyWordHandler.crawler(word, ipId);
-            Thread.sleep(NumberUtils.randomInt(50000, 100000));
 
-        } catch (Exception e) {
-            log.warn("keyWordHandler  failed: bookId:" + ipId + " word:" + word);
+        if (lastRuntime == null) {
+            lastRuntime = new Date();
+        }
+
+        int timeRange=NumberUtils.randomInt(8000,20000);
+        while (true) {
+            Date currentTime = new Date();
+            if (TimeUtils.diff(lastRuntime, currentTime) > (long) timeRange) {
+                lastRuntime = currentTime;
+                //获取类型
+                Integer type = jsonObject.getInteger("source");
+                Integer ipId = jsonObject.getInteger("bookId");
+                String word = jsonObject.getString("word");
+                try {
+                  keyWordHandler.crawler(word, ipId);
+                } catch (Exception e) {
+                    log.warn("keyWordHandler  failed: bookId:" + ipId + " word:" + word);
+                }
+
+                break;
+            }
         }
 
     }
