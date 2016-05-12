548fce0c9b6d5ddf2f27c9da4bb53c63a63307ec
jiangshan <yongxing@dreamy.live>
2016-05-12 16:15:18 +0800

    commit

--- servers/src/main/java/com/dreamy/service/impl/user/UserServiceImpl.java
+++ servers/src/main/java/com/dreamy/service/impl/user/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
