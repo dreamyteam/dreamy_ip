2cc096457c939a717c5b56a710e4a5531b02b49e
jiangshan <yongxing@dreamy.live>
2016-06-08 13:36:09 +0800

    commit

--- crawlerDemo/src/main/java/com/dreamy/crawler/handler/info/netbook/qd/QiDianMmHandler.java
+++ crawlerDemo/src/main/java/com/dreamy/crawler/handler/info/netbook/qd/QiDianMmHandler.java
@@ -78,14 +78,36 @@ public class QiDianMmHandler {
      * @param info
      * @param document
      */
+    /**
+     * 解析 作者链接
+     *
+     * @param info
+     * @param document
+     */
     private void getAuthorUrl(NetBookInfo info, Document document) {
+        try {
+            Elements elements = document.getElementsByAttributeValue("itemprop", "author");
+            if (elements != null && elements.size() > 0) {
+                Elements element = elements.first().getElementsByAttributeValue("itemprop", "url");
+                if (element != null) {
+                    String url = element.attr("href");
+                    String html = HttpUtils.getHtmlGet(url);
+                    Document urlDocument = Jsoup.parse(html);
+                    if (urlDocument != null) {
+                        Elements elements1 = urlDocument.getElementsByAttributeValue("title", "起点ID");
+                        if (elements1 != null && elements1.size() > 0) {
+                            Element element1 = elements1.first();
+                            String id = PatternUtils.getNum(element1.text());
+                            String authorUrl = "http://t.qdmm.com/Friend/HisFollower.php?id=" + id;
+                            info.setAuthorUrl(authorUrl);
+                        }
+                    }
+                }
 
-        Elements elements = document.getElementsByAttributeValue("itemprop", "author");
-        if (elements != null && elements.size() > 0) {
-            Elements element = elements.first().getElementsByAttributeValue("itemprop", "url");
-            if (element != null) {
-                info.setAuthorUrl(element.attr("href"));
             }
+        } catch (Exception e) {
+
+            log.error(" qidianmm 简介 is error book=" + info.getBookId(), e);
 
         }
 
---
+++ servers/src/main/java/com/dreamy/mogodb/beans/FanInfo.java
@@ -0,0 +1,26 @@
+package com.dreamy.mogodb.beans;
+
+/**
+ * Created by wangyongxing on 16/6/8.
+ */
+public class FanInfo {
+
+    private String sex;
+    private String address;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
