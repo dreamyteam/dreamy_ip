640dd08223df2e7f5896e66932cd5afdd48f8afd
jeff <jeff@dreamy.live>
2016-06-11 16:10:42 +0800

    update：更新时间

--- dashboard/src/main/java/com/dreamy/admin/tasks/rank/UpdateIndexTask.java
+++ dashboard/src/main/java/com/dreamy/admin/tasks/rank/UpdateIndexTask.java
@@ -89,7 +89,7 @@ public class UpdateIndexTask {
     private String newsSougouQueue;
 
 
-    @Scheduled(cron = "0 30 15 * * ?")
+    @Scheduled(cron = "0 30 16 * * ?")
     public void run() {
         LOGGER.info("start update rank job.." + TimeUtils.toString("yyyy-MM-dd HH:mm:ss", new Date()));
 
