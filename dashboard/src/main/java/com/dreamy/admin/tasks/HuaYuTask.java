c0e9e3acea392c5d4d3e69ea5dfce94cf69bc678
jiangshan <yongxing@dreamy.live>
2016-06-16 09:52:17 +0800

    commit

--- dashboard/src/main/java/com/dreamy/admin/tasks/HuaYuTask.java
+++ dashboard/src/main/java/com/dreamy/admin/tasks/HuaYuTask.java
@@ -1,12 +1,10 @@
 package com.dreamy.admin.tasks;
 
-import com.dreamy.beans.Page;
 import com.dreamy.service.mq.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,16 +12,19 @@ import java.util.Map;
  * Created by wangyongxing on 16/6/13.
  */
 @Component
-public class HuaYuTicketTask {
+public class HuaYuTask {
 
 
     @Value("${queue_crawler_huayu_ticket}")
     private String queueName;
-
     @Autowired
     private QueueService queueService;
 
-    @Scheduled(cron = "0 10 16 * * ?")
+
+    /***
+     * 花语月票 任务
+     */
+    @Scheduled(cron = "0 10 03 * * ?")
     public void run() {
         for (int i = 1; i < 156; i++) {
             Map<String, Integer> map = new HashMap<>();
@@ -32,18 +33,5 @@ public class HuaYuTicketTask {
         }
     }
 
-    public static void main(String[] args) {
-        Page page = new Page();
-        page.setPageSize(10);
-        int current = 1;
-        while (true) {
-            page.setCurrentPage(current);
-            page.setTotalNum(156);
-            System.out.println(page.getStartIndex() + "-");
-            if (!page.isHasNextPage()) {
-                break;
-            }
-            current++;
-        }
-    }
+
 }
---
+++ dashboard/src/main/java/com/dreamy/admin/tasks/QiDianTask.java
@@ -0,0 +1,61 @@
+package com.dreamy.admin.tasks;
+
+import com.dreamy.beans.Page;
+import com.dreamy.domain.ipcool.BookCrawlerInfo;
+import com.dreamy.enums.CrawlerSourceEnums;
+import com.dreamy.mogodb.beans.NetBookInfo;
+import com.dreamy.service.iface.ipcool.BookCrawlerInfoService;
+import com.dreamy.service.iface.mongo.NetBookInfoService;
+import com.dreamy.service.mq.QueueService;
+import com.dreamy.utils.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wangyongxing on 16/6/16.
+ */
+public class QiDianTask {
+
+    @Autowired
+    private QueueService queueService;
+    @Autowired
+    private BookCrawlerInfoService bookCrawlerInfoService;
+    @Autowired
+    private NetBookInfoService netBookInfoService;
+
+    @Value("${queue_crawler_qidian_fans}")
+    private String queueFanName;
+
+    /**
+     * 起点粉丝
+     */
+    @Scheduled(cron = "0 10 02 * * ?")
+    public void fans() {
+        BookCrawlerInfo entity = new BookCrawlerInfo().source(CrawlerSourceEnums.qidian.getType());
+        Page page = new Page();
+        page.setPageSize(200);
+        int current = 1;
+        while (true) {
+            page.setCurrentPage(current);
+            List<BookCrawlerInfo> list = bookCrawlerInfoService.getListByRecord(entity, page);
+            for (BookCrawlerInfo info : list) {
+                NetBookInfo netBookInfo = netBookInfoService.getById(info.getBookId());
+                Map<String, Object> map = new HashMap<>();
+                if (netBookInfo != null && StringUtils.isNotEmpty(netBookInfo.getAuthorUrl())) {
+                    map.put("url", netBookInfo.getAuthorUrl());
+                    map.put("bookId", info.getBookId());
+                    queueService.push(queueFanName, map);
+                }
+            }
+            if (!page.isHasNextPage()) {
+                break;
+            }
+            current++;
+        }
+    }
+}
