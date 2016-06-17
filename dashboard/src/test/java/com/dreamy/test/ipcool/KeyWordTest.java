ca850a2cdcd0886cd675c275fe4514d44fe43ef8
jiangshan <yongxing@dreamy.live>
2016-06-17 14:17:23 +0800

    commit

---
+++ dashboard/src/test/java/com/dreamy/test/ipcool/KeyWordTest.java
@@ -0,0 +1,7 @@
+package com.dreamy.test.ipcool;
+
+/**
+ * Created by wangyongxing on 16/6/17.
+ */
+public class KeyWordTest {
+}
--- servers/src/main/java/com/dreamy/service/impl/ipcool/RankServiceImpl.java
+++ servers/src/main/java/com/dreamy/service/impl/ipcool/RankServiceImpl.java
@@ -31,13 +31,10 @@ public class RankServiceImpl implements RankService {
             commonParams.put("key", "book:update:" + bookView.getBookId());
             commonParams.put("isbn", ipBook.getCode());
             commonParams.put("operation", action);
-            commonParams.put("tieba_keyword", ipBook.getTiebaKeyword());
-            commonParams.put("index_keyword", ipBook.getIndexKeyword());
+//            commonParams.put("tieba_keyword", ipBook.getTiebaKeyword());
+//            commonParams.put("index_keyword", ipBook.getIndexKeyword());
 //            commonParams.put("news_keyword", ipBook.getNewsKeyword());
-//            commonParams.put("search_keyword", ipBook.getSearchKeyword());
-            commonParams.put("news_keyword", "《" + bookView.getName() + "》 " + bookView.getAuthor());
-            commonParams.put("search_keyword", "《" + bookView.getName() + "》 " + bookView.getAuthor());
-
+            commonParams.put("search_keyword", ipBook.getSearchKeyword());
         }
 
 
